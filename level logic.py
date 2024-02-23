a9243d2ff88ae97d0da65c51dd7035045d7d1a5f
Ilon Maks <mekarginov@gmail.com>
2024-02-23 19:36:22 +0500

    Недописаная логика уровня

--- level logic.py
+++ level logic.py
@@ -1,3 +1,20 @@
 lvlnmb=5
+#Время перезарядки у всех персов одинаковое
+atack_time_norm=0.1
 if lvlnmb==5:
-    print('ГЭЙЭ')
+    #Характеристики врага(в данном случае босс=Шнюк)
+    health_agro=1000
+    atack_agro=50
+    atcack_time_agro=0.3
+while ():
+    #Здесь должно быть окно выбора перса
+    if charachter=='Luntik' and sk_charachter=='Дарт Лунтус':
+        health_norm=5000
+        shield_norm=100
+        atack_norm=300
+        super_norm=1000
+        super_time_norm=20.0
+    
+    
+
+    
