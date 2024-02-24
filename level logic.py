c982aa20ae2da1a06d62000113fcd287fd902473
Ilon Maks <mekarginov@gmail.com>
2024-02-24 18:39:51 +0500

    писал логику уровня Женя Ерафим Настя Яков _ Генадий Евкакий Йода

--- level logic.py
+++ level logic.py
@@ -1,3 +1,5 @@
+import time
+import pygame
 lvlnmb=5
 #Время перезарядки у всех персов одинаковое
 atack_time_norm=0.1
@@ -6,15 +8,38 @@ if lvlnmb==5:
     health_agro=1000
     atack_agro=50
     atcack_time_agro=0.3
+#Здесь должно быть окно выбора перса
+if charachter=='Лунтик' and sk_charachter=='Дарт Лунтус':
+    health_norm=5000
+    shield_norm=100
+    atack_norm=300
+    super_norm=1000
+    super_time_norm=20.0
+if charachter=='Деда Шер' and sk_charachter=='Киборг Шер':
+    health_norm=3000
+    shield_norm=35
+    atack_norm=50
+    super_norm=385
+    super_time_norm=15.0
+if charachter=='Баба Капа' and sk_charachter=='Учитель по информатеке Капа':
+    health_norm=3500
+    shield_norm=75
+    atack_norm=65
+    super_norm=500
+    super_time_norm=17.0
+if charachter=='Пчела мен' and sk_charachter=='Антивирус Пчела мен':
+    health_norm=2750
+    shield_norm=80
+    atack_norm=40
+    #У этого скина ульта-хилить
+    super_norm=75
+    super_time_norm=15.0
+#Цикл уровня
 while ():
-    #Здесь должно быть окно выбора перса
-    if charachter=='Luntik' and sk_charachter=='Дарт Лунтус':
-        health_norm=5000
-        shield_norm=100
-        atack_norm=300
-        super_norm=1000
-        super_time_norm=20.0
     
+
+
+
     
 
     
