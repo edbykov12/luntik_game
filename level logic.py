43d4158973f1a300adc90c9a7756862ec67c5e2f
Ilon Maks <mekarginov@gmail.com>
2024-03-03 20:10:01 +0500

    я не помню что я делал P.S-ааааааааа... эта болезнь меня уже заколебала

--- level logic.py
+++ level logic.py
@@ -7,20 +7,12 @@ f = open('checkpoint.txt', 'r')
 e = f.read()
 f.close()
 pygame.init()
-lvlnmb=e
-#Время перезарядки у всех персов одинаковое
-atack_time_norm=0.1
-if lvlnmb==e:
-    #Характеристики врага(в данном случае босс=Шнюк)
-    health_agro=1000
-    atack_agro=50
-    atcack_time_agro=0.3
-#Здесь должно быть окно выбора перса
+lvlnmb=5
 #Пока выбор персонажа будет просто переменная чтобы не выдавыло ошибок
 sk_charachter='Дарт Лунтус'
 charachter='Лунтик'
 if charachter=='Лунтик' and sk_charachter=='Дарт Лунтус':
-    health_norm=5000
+    health_norm=10
     shield_norm=100
     atack_norm=300
     super_norm=1000
@@ -44,6 +36,17 @@ if charachter=='Пчела мен' and sk_charachter=='Антивирус Пче
     #У этого скина ульта-хилить
     super_norm=75
     super_time_norm=15.0
+#Время перезарядки у всех персов одинаковое
+atack_time_norm=0.1
+
+if lvlnmb==5:
+    #Характеристики врага(в данном случае босс=Шнюк)
+    health_agro=1000
+    atack_agro=50
+    atcack_time_agro=0.3
+    #Загрузка фото врага
+    image = pygame.image.load('шнюк (обычный).png')
+#Здесь должно быть окно выбора перса
 #Цикл уровня
 f = open('checkpoint.txt', 'r')
 e = f.read()
@@ -69,10 +72,17 @@ pygame.mixer.music.play()
 #Цикл уровня
 while 1:
     for i in pygame.event.get():
+        screen.blit(image, (700, 700))
         f1 = pygame.font.Font(None, 30)
         text1 = f1.render(str(health_agro), 1, (0, 0, 0))
         gameScreen.blit(text1, (5, 50))
         pygame.display.update()
+        if 'a'=='a':
+            if shield_norm>=atack_agro:
+                continue
+            else:
+                health_norm-=(shield_norm-atack_agro)
+            
         if health_agro<=0:
             f1 = pygame.font.Font(None, 36)
             text1 = f1.render('Поздравляю! Победа!', 1, (180, 0, 0))
@@ -80,19 +90,24 @@ while 1:
             pygame.display.update()
             pygame.time.delay(600)
             sys.exit()
+        if i.type==pygame.KEYDOWN:
+            if i.key== pygame.K_w:
+                health_agro-=atack_norm
+        if i.type==pygame.KEYDOWN:
+            if i.key== pygame.K_s:
+                health_norm=health_norm-atack_agro
         if health_norm<=0:
             music4=pygame.mixer.music.load("ДИМОООН-ДИМОООН_(muzmo.su).mp3")
             pygame.mixer.music.play()
             f2 = pygame.font.Font(None, 36)
             text2 = f2.render('К сожалению вы проиграли...', 1, (180, 0, 0))
             gameScreen.blit(text2, (10, 50))
-            wait(6000)
+            pygame.display.update()
+            pygame.time.delay(7000)
             sys.exit()
         if i.type == pygame.QUIT:
             sys.exit()
-        elif i.type==pygame.KEYDOWN:
-            if i.key== pygame.K_w:
-                health_agro=health_agro-atack_norm
+
     
 
 
